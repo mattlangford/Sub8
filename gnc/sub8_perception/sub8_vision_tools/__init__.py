4e846f57628170f0fcb0e8b38d061e5d51cc108b
Matt Langford <matthew.langford95@gmail.com>
2017-01-22 20:52:48 -0500

    VISION_TOOLS: Cleanup directory

--- gnc/sub8_perception/sub8_vision_tools/__init__.py
+++ gnc/sub8_perception/sub8_vision_tools/__init__.py
@@ -1,8 +1,7 @@
 # flake8: noqa
-import visual_threshold_tools
-import threshold_tools
 from estimation import ProjectionParticleFilter
 from multi_observation import MultiObservation
 from marker_occ_grid import MarkerOccGrid
-import machine_learning
+import machine_learning_2016
 import labelling
+import thresholding
--- gnc/sub8_perception/sub8_vision_tools/machine_learning_2016/__init__.py
+++ gnc/sub8_perception/sub8_vision_tools/machine_learning_2016/__init__.py
@@ -1,2 +1,3 @@
 from utils import balanced_resample, desample_binary
-import boost
+import boosting
+import labelling
