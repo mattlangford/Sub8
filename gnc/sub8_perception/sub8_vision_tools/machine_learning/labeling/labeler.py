80a6a0bb2c700921c71aa2b93d68497900df474f
Matt Langford <matthew.langford95@gmail.com>
2017-01-24 22:27:45 -0500

    LABELER: Update base labeler, add labeler interface

--- gnc/sub8_perception/sub8_vision_tools/machine_learning/labeling/labeler.py
+++ gnc/sub8_perception/sub8_vision_tools/machine_learning/labeling/labeler.py
@@ -5,43 +5,17 @@ from sub8_tools import text_effects
 
 
 image_providers = {'test': "test", 'test2': "test2"}
-labelers = {'test': "test", 'test2': "test2"}
+p = text_effects.Printer()
 
 
 '''
 A main interface for various labeling tools.
-Below are a couple base classes to inherit from in order to provide the
-    correct methods for this interface script to do it's magic.
 '''
 
 # Base Classes ================================================================
 
-class BaseLabeler(object):
-    '''
-    Labeller classes should provide these methods. Exaples include:
-        - Simple polygon labeler
-        - Simple circle labeler
-        - Paint brush labeler
-    '''
-    def set_image(self, img, pre_mask=None):
-        ''' Load the image to be labeled 
-            img       := numpy array of an image
-            pre_mask  := numpy array of a pre loaded mask 
-        '''
-        self.image = img.astype(np.uint8)
-        self.mask = pre_mask.astype(np.uint8)
-
-    def start_labelling(self):
-        '''Start the labeling process'''
-        key = None
-        while key is not ord('q'):
-            # Do image canvas drawing and segmenting, breaking on 'q'
-            key = cv2.waitKey(10) & 0xFF 
-
-
 class BaseImageProvider(object):
-    '''
-    Classes that provide image sources. Examples may include:
+    '''Classes that provide image sources. Examples may include:
         - A bag crawler
         - A video crawler
         - Loading images from a folder
@@ -54,9 +28,89 @@ class BaseImageProvider(object):
         ''' Returns the next image in the sequence '''
         pass
 
-# ==============================================================================
 
-p = text_effects.Printer()
+class BaseLabeler(object):
+    '''Used by the LabelerInterface to eaisly switch between different labelers.
+    '''
+    def __init__(self, draw_data):
+        # Stores data used by this class. Do not interact directly with or things could break
+        self._draw_data = draw_data
+
+    @property
+    def image(self):
+        ''' Gets the current image being segmeneted '''
+        return self._draw_data.get('image', None)
+
+    @property
+    def mask(self):
+        ''' Gets the current mask for this segmentation '''
+        return self._draw_data.get('mask', None)
+
+    @mask.setter
+    def mask(self, mask):
+        ''' Sets the current mask for this segmentation '''
+        self._draw_data['mask'] = mask
+
+    @property
+    def overlay(self):
+        ''' Gets the current overlay for this segmentation '''
+        return self._draw_data['overlay']
+    
+    @overlay.setter
+    def overlay(self, overlay):
+        ''' Sets the current overlay for this segmentation '''
+        self._draw_data['overlay'] = overlay
+
+    def set_active(self):
+        ''' Method called when this labeler is now the active one '''
+        self.overlay = np.zeros(shape=self.image.shape)
+
+    def mouse_cb(self, event, x, y, flags, param):
+        ''' cv2 callback to be called for this labeler when it is active '''
+        pass
+
+
+# General Labeler interface ===================================================
+
+class LabelerInterface():
+    '''Provides tools to create polygon segments'''
+    def __init__(self, *args, **kwargs):
+        blank = np.zeros(shape=(480, 640))
+        self._draw_data = {'image': blank, 'mask': blank, 'overlay': blank}
+
+        # Maps shortcut keys to labelers
+        self._labelers = {'b': BaseLabeler}
+        self._active_labeler = None
+
+        self._name = "labeler"
+        cv2.namedWindow(self._name, cv2.WINDOW_NORMAL)
+        cv2.setMouseCallback(self._name, self._mouse_cb)
+    
+    def _mouse_cb(self, event, x, y, flags, param):
+        ''' Handles drawing and what not '''
+        print "cb" 
+
+    def _apply_layers(self, layers):
+        _disp_img = np.copy(self.image)
+        for layer in layers
+            _disp_img += layer
+
+        return _disp_img
+
+    def _print_options()
+
+    def start_labelling(self):
+        key = None
+        if self._active_labeler is None:
+
+            return
+
+        while key is not ord('q'):
+             
+            cv2.imshow() 
+            key = cv2.waitKey(10) & 0xFF
+
+# =============================================================================
 
 def _menu_options(options, name=None):
     ''' Returns the object associated with user selected entry
@@ -102,4 +156,3 @@ if __name__ == "__main__":
     print p.bold("\nWelcome to the image labeler!")
 
     image_provider = _menu_options(image_providers, "image providers")
-    labeler = _menu_options(labelers, "labelers")
